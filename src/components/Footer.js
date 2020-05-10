165327d10ec37bb695f1d7391cc02fbb7b4c8bcc
chizzy <cwchiz16@gmail.com>
2020-05-09 21:03:31 -0600

    erased unused pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -34,11 +34,7 @@ const Footer = class extends React.Component {
                         About
                       </Link>
                     </li>
-                    <li>
-                      <Link className="navbar-item" to="/products">
-                        Products
-                      </Link>
-                    </li>
+                   
                     <li>
                       <Link className="navbar-item" to="/contact/examples">
                         Form Examples
@@ -60,11 +56,7 @@ const Footer = class extends React.Component {
               <div className="column is-4">
                 <section>
                   <ul className="menu-list">
-                    <li>
-                      <Link className="navbar-item" to="/blog">
-                        Latest Stories
-                      </Link>
-                    </li>
+                    
                     <li>
                       <Link className="navbar-item" to="/contact">
                         Contact
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -4,7 +4,7 @@ import { Link, graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
 import Features from '../components/Features'
-import BlogRoll from '../components/BlogRoll'
+// import BlogRoll from '../components/BlogRoll'
 
 export const IndexPageTemplate = ({
   image,
@@ -88,22 +88,14 @@ export const IndexPageTemplate = ({
                 </div>
                 <Features gridItems={intro.blurbs} />
                 <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
-                      See all products
-                    </Link>
-                  </div>
+                 
                 </div>
                 <div className="column is-12">
                   <h3 className="has-text-weight-semibold is-size-2">
                     Latest stories
                   </h3>
-                  <BlogRoll />
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog">
-                      Read more
-                    </Link>
-                  </div>
+                  {/* <BlogRoll /> */}
+               
                 </div>
               </div>
             </div>
